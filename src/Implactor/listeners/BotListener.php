87bef67337ae3b233c06098f41f6e5e813ae60ed
Zadey <41181506+Zadezter@users.noreply.github.com>
2018-08-11 05:15:57 +0000

    Fixed message.

--- src/Implactor/listeners/BotListener.php
+++ src/Implactor/listeners/BotListener.php
@@ -66,7 +66,7 @@ class BotListener implements Listener {
 					$packetSwing->entityRuntimeId = $entity->getId();
 					$packetSwing->action = SwingPacket::ACTION_SWING_ARM;
 					$damager->sendDataPacket($packetSwing);
-					$damager->sendMessage("§7[§bBot§7] ". $this->getLang("bot-no-hit-damage-message"));
+					$damager->sendMessage("§7[§bBot§7] ". $this->plugin->getLang("bot-no-hit-damage-message"));
 				}
 		  }
 	}
